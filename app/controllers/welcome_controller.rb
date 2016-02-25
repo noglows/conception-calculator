c9cfe04ee4dce1993e097aa315bbe1893ac5e5ab
noglows <jessica.noglows@outlook.com>
2016-02-25 10:46:04 -0800

    Updated seed data - manually reviewed data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.where(year: 1991)
+    @events = Event.all
   end
 
   def songs
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,58 +8,88 @@
 
 # puts 'EMPTY THE MONGODB DATABASE'
 # ::Mongoid::Sessions.default.drop
-# years = (1900..2015).to_a
-# years.each do |year|
-#   File.open("./wiki_data/#{year}_ready.txt", "r").each do |line|
-#     a = line.split(", ")
-#     check = 3
-#     info = ""
-#     while (a[check] != "true") && (a[check] != "false")
-#       info += " "
-#       info += a[check]
-#       check += 1
-#     end
-#     if a[check] == "true"
-#       a[check] = true
-#     elsif a[check] == "false"
-#       a[check] = false
-#     end
-#     if a[check+1] == "true"
-#       a[check+1] = true
-#     elsif a[check+1] == "false"
-#       a[check+1] = false
-#     end
-#
-#     a[check+3].slice!("\n")
-#
-#     event = Event.create! :year => a[0].to_i, :month => a[1], :day => a[2], :info => info, :on_going => a[check], :is_range => a[check+1], :end_month => a[check+2], :end_day => a[check+3]
-#     puts "New event created: " << event.info
-#     event.save
-#   end
-# end
+years = (1902..2015).to_a
+years.each do |year|
+  File.open("./wiki_data/#{year}_ready.txt", "r").each do |line|
+    a = line.split(", ")
+    check = 3
+    info = ""
+    while (a[check] != "true") && (a[check] != "false")
+      info += " "
+      info += a[check]
+      check += 1
+    end
+    if a[check] == "true"
+      a[check] = true
+    elsif a[check] == "false"
+      a[check] = false
+    end
+    if a[check+1] == "true"
+      a[check+1] = true
+    elsif a[check+1] == "false"
+      a[check+1] = false
+    end
 
+    if a[check+3] != nil
+      a[check+3].slice!("\n")
+    end
+
+    event = Event.create! :year => a[0].to_i, :month => a[1], :day => a[2], :info => info, :on_going => a[check], :is_range => a[check+1], :end_month => a[check+2], :end_day => a[check+3]
+    puts "New event created: " << event.info
+    event.save
+  end
+end
+
+File.open("./wiki_data/error_file.txt", "r").each do |line|
+  a = line.split(", ")
+  check = 3
+  info = ""
+  while (a[check] != "true") && (a[check] != "false")
+    info += " "
+    info += a[check]
+    check += 1
+  end
+  if a[check] == "true"
+    a[check] = true
+  elsif a[check] == "false"
+    a[check] = false
+  end
+  if a[check+1] == "true"
+    a[check+1] = true
+  elsif a[check+1] == "false"
+    a[check+1] = false
+  end
+
+  if a[check+3] != nil
+    a[check+3].slice!("\n")
+  end
+
+  event = Event.create! :year => a[0].to_i, :month => a[1], :day => a[2], :info => info, :on_going => a[check], :is_range => a[check+1], :end_month => a[check+2], :end_day => a[check+3]
+  puts "Created error event " << event.info
+  event.save
+end
 #music_years = (1940..2016).to_a
 # years.each do |year|
 #   File.open("./wiki_data/#{year}_ready.txt", "r").each do |line|
 
-music_csv = CSV.read("./wiki_data/all_music_data.csv")
-# Create a new market object for each row in the CSV
-music_csv.each do |year, date, title, artist|
-  a = date.split(" ")
-  month = a[0]
-  day = a[1]
-  if title == nil
-    title = @prev_title
-  else
-    @prev_title = title
-  end
-  if artist == nil
-    artist = @prev_artist
-  else
-    @prev_artist = artist
-  end
-  song = Song.create! :year => year.to_i, :month => month, :day => day, :title => title, :artist => artist
-  song.save
-end
+# music_csv = CSV.read("./wiki_data/all_music_data.csv")
+# # Create a new market object for each row in the CSV
+# music_csv.each do |year, date, title, artist|
+#   a = date.split(" ")
+#   month = a[0]
+#   day = a[1]
+#   if title == nil
+#     title = @prev_title
+#   else
+#     @prev_title = title
+#   end
+#   if artist == nil
+#     artist = @prev_artist
+#   else
+#     @prev_artist = artist
+#   end
+#   song = Song.create! :year => year.to_i, :month => month, :day => day, :title => title, :artist => artist
+#   song.save
+# end
 
 #end
