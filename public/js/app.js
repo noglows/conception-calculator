1949653dafd387e7771c53b5e7bbac244dc1a787
noglows <jessica.noglows@outlook.com>
2016-02-29 12:15:37 -0800

    Events are pulled for conception date

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,11 @@ window.requestAnimationFrame(function () {
       data: "birthday=" + output_as_date,
     })
     .done(function(data) {
-      start_date = data[0]
-      end_date = data[1]
-      start_date = start_date.split("-")
+      start_date = data[0];
+      end_date = data[1];
+      start_date = start_date.split("-");
       start_date = start_date[1] + "/" + start_date[2] + "/" + start_date[0];
-      end_date = end_date.split("-")
+      end_date = end_date.split("-");
       end_date = end_date[1] + "/" + end_date[2] + "/" + end_date[0];
       $.ajax({
         method: "GET",
@@ -22,7 +22,13 @@ window.requestAnimationFrame(function () {
         data: "start=" + start_date + "&end=" + end_date,
       })
       .done(function(data) {
-          console.log(data[0]);
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+          $("#events").append("<p>" + data[i].info + "</p>");
+        }
+          // console.log(data[0].info);
+          // console.log($("#events").html())
+          // $("#events").html("<p>" + data[0].info + "</p>");
       });
     });
   });
