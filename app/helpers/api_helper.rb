1ff12763b05542d545e99af2d4370914b4680b82
noglows <jessica.noglows@outlook.com>
2016-02-28 19:35:33 -0800

    Wrote leap year logic

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -11,4 +11,24 @@ module ApiHelper
     year = split_date[2].to_i
     return month, day, year
   end
+
+  def create_range(start_date, end_date)
+    start_month, start_day, start_year = ApiController.helpers.date_splitter(start_date)
+    end_month, end_day, end_year = ApiController.helpers.date_splitter(end_date)
+    days_in_month = {"January" => 31, "February" => 28, "March" => 31, "April" => 30, "May" => 31, "June" => 30, "July" => 31, "August" => 31, "September" => 30, "October" => 31 }
+    days_in_month["November"] = 30
+    days_in_month["December"] = 31
+  end
+
+  def check_leap_year(year)
+    if year % 4 != 0
+      return false
+    elsif year % 100 !=0
+      return true
+    elsif year % 400 != 0
+      return false
+    else
+      return true
+    end
+  end
 end
