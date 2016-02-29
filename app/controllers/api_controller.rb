f7c24a4aabffb26604705fc5164d3193df079e88
noglows <jessica.noglows@outlook.com>
2016-02-29 10:41:41 -0800

    Wrote movie range and song range methods

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -55,6 +55,49 @@ class ApiController < ApplicationController
     end
   end
 
-  #@events = Event.where(:year.gte => 2001)
+  def movies_in_range
+    respond_to do |format|
+      format.json do
+        start_date = params[:start]
+        end_date = params[:end]
+        start_month, start_day, start_year = ApiController.helpers.date_splitter(start_date)
+        end_month, end_day, end_year = ApiController.helpers.date_splitter(end_date)
+
+        start_date = Date.new(start_year, start_month, start_day)
+        end_date = Date.new(end_year, end_month, end_day)
+        date_range = ApiController.helpers.create_range(start_date, end_date)
+        movies = []
+        date_range.each do |date|
+          date_comp = date.split("-")
+          movies.push(Movie.where(year: date_comp[0].to_i, month: date_comp[1].to_i, day: date_comp[2].to_i))
+        end
+        movies.flatten!
+        render json: [movies]
+      end
+    end
+  end
+
+  def songs_in_range
+    respond_to do |format|
+      format.json do
+        start_date = params[:start]
+        end_date = params[:end]
+        start_month, start_day, start_year = ApiController.helpers.date_splitter(start_date)
+        end_month, end_day, end_year = ApiController.helpers.date_splitter(end_date)
+
+        start_date = Date.new(start_year, start_month, start_day)
+        end_date = Date.new(end_year, end_month, end_day)
+        date_range = ApiController.helpers.create_range(start_date, end_date)
+        songs = []
+        date_range.each do |date|
+          date_comp = date.split("-")
+          songs.push(Song.where(year: date_comp[0].to_i, month: date_comp[1].to_i, day: date_comp[2].to_i))
+        end
+        songs.flatten!
+        render json: [songs]
+      end
+    end
+  end
+
 
 end
