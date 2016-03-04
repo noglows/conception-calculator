012ced5588c5e7b489cadee6c57dad93416ebe95
noglows <jessica.noglows@outlook.com>
2016-03-03 20:24:35 -0800

    Working Mongoid models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,15 @@
 class Movie
-  # include Mongoid::Document
-  # field :year, type: Integer
-  # field :month, type: Integer
-  # field :day, type: Integer
-  # field :title, type: String
+  include Mongoid::Document
+  field :year, type: Integer
+  field :month, type: Integer
+  field :day, type: Integer
+  field :title, type: String
 
-  include Dynamoid::Document
-  table :name => :Movies
-  field :year, :integer
-  field :month, :integer
-  field :day, :integer
-  field :title, :string
+  # include Dynamoid::Document
+  # table :name => :Movies
+  # field :year, :integer
+  # field :month, :integer
+  # field :day, :integer
+  # field :title, :string
 
 end
