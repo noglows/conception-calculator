e123ff02269204b377ece980240974fb08da90e1
noglows <jessica.noglows@outlook.com>
2016-03-09 12:03:18 -0800

    FB/Twitter debug

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,4 @@
 window.requestAnimationFrame(function () {
-  console.log(window.location.href);
   if ((window.location.href != "http://localhost:3000/") && (window.location.href != "http://www.xmarksyourstart.com/")) {
     var url = window.location.href;
     url = url.replace("http://localhost:3000/", "");
@@ -103,15 +102,23 @@ window.requestAnimationFrame(function () {
 
       $(".data-returned").show();
 
+      //href="http://www.facebook.com/sharer.php?s=100&p[title]=YOUR_TITLE&p[summary]=YOUR_SUMMARY&p[url]=YOUR_URL&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT"
+
       var fb_a = '<a href="https://www.facebook.com/sharer/sharer.php?u=www.xmarksyourstart.com/';
-      var fb_b = '&t=TITLE" onclick="javascript:window.open(this.href,';
+      var fb_b = '&p[summary]=YOUR_SUMMARY" onclick="javascript:window.open(this.href,';
       var fb_c =" ''";
       var fb_d = ", 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;";
       var fb_e = '" target="_blank" title="Share on Facebook">Share on Facebook</a> ';
 
       var facebook_html = fb_a + comb_output + fb_b + fb_c + fb_d + fb_e;
 
-      var twitter_html = '<a target="_blank" href="http://twitter.com/share?text=Want%20to%20be%20grossed%20out?%20%20Check%20out%20these%20things%20that%20may%20have%20led%20to%20my%20conception.&url=http://www.xmarksyourstart.com/'+ comb_output +'">Share This on Twitter</a>';
+      var tw_a = '<a href="http://twitter.com/share?text=Want%20to%20be%20grossed%20out?%20%20Check%20out%20these%20things%20that%20may%20have%20led%20to%20my%20conception.&url=http://www.xmarksyourstart.com/';
+      var tw_b = '" onclick="javascript:window.open(this.href,';
+      var tw_e = '" target="_blank" title="Share on Twitter">Share on Twitter</a> ';
+
+      var twitter_html = tw_a + comb_output + tw_b + fb_c + fb_d + tw_e;
+
+      //var twitter_html = '<a target="_blank" href="http://twitter.com/share?text=Want%20to%20be%20grossed%20out?%20%20Check%20out%20these%20things%20that%20may%20have%20led%20to%20my%20conception.&url=http://www.xmarksyourstart.com/'+ comb_output +'">Share This on Twitter</a>';
 
       $(".facebook").append(facebook_html);
       $(".twitter").append(twitter_html);
