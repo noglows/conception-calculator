c790bd1c4fd9c580e3f65dedd611c13493138334
noglows <jessica.noglows@outlook.com>
2016-03-09 11:14:56 -0800

    Facebook share is working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,9 @@
 window.requestAnimationFrame(function () {
   console.log(window.location.href);
   if ((window.location.href != "http://localhost:3000/") && (window.location.href != "http://www.xmarksyourstart.com/")) {
-    console.log("I'm in here");
     var url = window.location.href;
     url = url.replace("http://localhost:3000/", "");
     url = url.replace("http://www.xmarksyourstart.com/", "");
-    console.log(url);
     var year = url.substring(0,4);
     var month = url.substring(4,6);
     var day = url.substring(6,8);
@@ -105,19 +103,23 @@ window.requestAnimationFrame(function () {
 
       $(".data-returned").show();
 
-      var facebook_html = '<a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=#www.xmarksyourstart/'+ comb_output + '">Share on Facebook</a>';
-      var twitter_html = '<a target="_blank" href="http://twitter.com/share?text=Learn%20about%20my%20conception!&url=http://www.xmarksyourstart.com/'+ comb_output +'">Share This on Twitter</a>';
+      var fb_a = '<a href="https://www.facebook.com/sharer/sharer.php?u=www.xmarksyourstart.com/';
+      var fb_b = '&t=TITLE" onclick="javascript:window.open(this.href,';
+      var fb_c =" ''";
+      var fb_d = ", 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;";
+      var fb_e = '" target="_blank" title="Share on Facebook">Share on Facebook</a> ';
+
+      var facebook_html = fb_a + comb_output + fb_b + fb_c + fb_d + fb_e;
+
+      var twitter_html = '<a target="_blank" href="http://twitter.com/share?text=Want%20to%20be%20grossed%20out?%20%20Check%20out%20these%20things%20that%20may%20have%20led%20to%20my%20conception.&url=http://www.xmarksyourstart.com/'+ comb_output +'">Share This on Twitter</a>';
 
       $(".facebook").append(facebook_html);
       $(".twitter").append(twitter_html);
-
-
 
       $("#events").empty();
       $("#songs").empty();
       $("#movies").empty();
       $("#conceptionRange").empty();
-
 
       var unusual = ($("#checkUnusual:checkbox:checked").length > 0);
       var number = $('#actualBirthInput').val();
